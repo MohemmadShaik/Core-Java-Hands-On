0acb45871b18a67db6ca6f7a2249844b0d5ac6ab
MohemmadShaik <mohemmadshaik@gmail.com>
2026-01-10 00:00:15 +0530

    Revision of Day 6 String concepts of MCQ Hands on/

---
+++ Day6_Strings.java
@@ -0,0 +1,22 @@
+public class Day6_Strings {
+    public static void main(String[] args) {
+
+        // Q1
+        /*
+         * String s1 = "Java"; //literal stored in SCP
+         * String s2 = "Java"; //literal stored in SCP
+         * String s3 = new String("Java"); //stored in heap
+         * System.out.println(s1==s2); // o/p : true
+         * System.out.println(s3==s1); // o/p : false
+         */
+
+        // Q2
+        /* String s = "Hello";
+        s.concat(" World"); //Since it's not assigned we loose
+        System.out.println(s); //o/p : Hello */
+
+        //Q3
+        
+    }
+
+}
